99bc279f0e9c1aac273c5c58ec83b96ae781910e
Kishi <m.cankisi@gmail.com>
2023-02-13 15:08:48 +0000

    first django project with routing to one app called expenses

---
+++ expenses/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, "../templates/index.html")
+
+def add_expense(request):
+    return render(request, "../templates/add-expense.html")
